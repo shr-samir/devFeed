a0a88e7f57d0046b5a9330b3cba5e86a9fc4fe69
Samir Shrestha <linkwithsamir@gmail.com>
2026-05-17 06:29:53 +0545

    feat(app): wire provider tree and navigation root in App.tsx

---
+++ App.tsx
@@ -0,0 +1,20 @@
+// App.tsx
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import RootNavigator from "@/navigation/RootNavigator";
+import { queryClient } from "@/shared/lib/queryClient";
+import { persistor, store } from "@/store";
+
+export default function App() {
+	return (
+		<Provider store={store}>
+			<PersistGate persistor={persistor} loading={null}>
+				<QueryClientProvider client={queryClient}>
+					<RootNavigator />
+				</QueryClientProvider>
+			</PersistGate>
+		</Provider>
+	);
+}
